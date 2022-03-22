d5b8953435d5571bb9107556ae020c2d76671055
Inese Upelniece <inese.upelniece@team11.lv>
2022-03-22 14:52:10 +0200

    added register and unregister commands

--- src/NXAExtendedRpc/NXAExtendedRpc.CNR.cs
+++ src/NXAExtendedRpc/NXAExtendedRpc.CNR.cs
@@ -7,6 +7,7 @@ using Neo.SmartContract.Native;
 using Neo.VM;
 using Neo.VM.Types;
 using Neo.Wallets;
+using Nxa.Plugins.HelperObjects;
 using System;
 using System.Numerics;
 using System.Security.Cryptography;
@@ -25,6 +26,7 @@ namespace Nxa.Plugins
         {
             string cname = _params[0].ToString();
             var result = new JObject();
+            result["cname"] = cname;
 
             if (Utility.IsCNRAddress(cname))
             {
@@ -37,6 +39,108 @@ namespace Nxa.Plugins
                 result["error"] = "Input value is invalid - it should be either email or alphanumeric value ending with .id.dvita.com";
             }
             return result;
+        }
+
+        /// <summary>
+        /// Process "register" command
+        /// </summary>
+        [RpcMethod]
+        protected virtual JObject Register(JArray _params)
+        {
+            string cname = _params[0].ToString();
+            UInt160 address = _params[1].ToScriptHash(system.Settings);
+            string privateKey = _params[2].AsString();
+            var result = new JObject();
+            result["cname"] = cname;
+            result["address"] = address.ToString();
+
+            if (Utility.IsCNRAddress(cname))
+            {
+                RegisterAddressByCNR(cname, address, privateKey, system, ref result);
+            }
+            else
+            {
+                result["error"] = "CNR name input is invalid - it should be either email or alphanumeric value ending with .id.dvita.com";
+            }
+
+            return result;
+        }
+
+        internal static bool RegisterAddressByCNR(string cname, UInt160 address, string signer, NeoSystem system, ref JObject result)
+        {
+            KeyPair key = Utility.GetKeyPair(signer);
+            OperationAccount account = new OperationAccount(key, system.Settings);
+            OperationWallet wallet = new OperationWallet(system.Settings, new OperationAccount[] { account });
+
+            try
+            {
+                byte[] script;
+                using (ScriptBuilder scriptBuilder = new ScriptBuilder())
+                {
+                    var scriptHash = scriptHashStr.ToScriptHash(system.Settings.AddressVersion);
+                    scriptBuilder.EmitDynamicCall(scriptHash, "register", cname, address);
+                    script = scriptBuilder.ToArray();
+                }
+                var transactionResult= Operations.CreateSendTransaction(system: system, script: script, wallet: wallet, account: key.PublicKeyHash);
+                result["result"] = transactionResult;
+                return true;
+            }
+            catch (Exception e)
+            {
+                result["error"] = $"Error: {e.Message}: {e.StackTrace}.";
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Process "unregister" command
+        /// </summary>
+        [RpcMethod]
+        protected virtual JObject Unregister(JArray _params)
+        {
+            string cname = _params[0].ToString();
+            string privateKey = _params[1].AsString();
+            var result = new JObject();
+            result["cname"] = cname;
+
+            if (Utility.IsCNRAddress(cname))
+            {
+                UnregisterAddressByCNR(cname, privateKey, system, ref result);
+            }
+            else
+            {
+                result["error"] = "CNR name input is invalid - it should be either email or alphanumeric value ending with .id.dvita.com";
+            }
+
+            return result;
+        }
+
+        internal static bool UnregisterAddressByCNR(string cname, string signer, NeoSystem system, ref JObject result)
+        {
+            KeyPair key = Utility.GetKeyPair(signer);
+            OperationAccount account = new OperationAccount(key, system.Settings);
+            OperationWallet wallet = new OperationWallet(system.Settings, new OperationAccount[] { account });
+
+            try
+            {
+                byte[] script;
+                using (ScriptBuilder scriptBuilder = new ScriptBuilder())
+                {
+                    var scriptHash = scriptHashStr.ToScriptHash(system.Settings.AddressVersion);
+                    scriptBuilder.EmitDynamicCall(scriptHash, "unregister", cname);
+                    script = scriptBuilder.ToArray();
+                }
+                var transactionResult = Operations.CreateSendTransaction(system: system, script: script, wallet: wallet, account: key.PublicKeyHash);
+                result["result"] = transactionResult;
+                return true;
+            }
+            catch (Exception e)
+            {
+                result["error"] = $"Error: {e.Message}: {e.StackTrace}.";
+            }
+
+            return false;
         }
 
         private UInt160 ResolveAddressByCNR(NeoSystem neoSystem, string cname)
